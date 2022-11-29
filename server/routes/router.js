c37b30c0a0c967bec16b25b40d980d6c72f35935
markhannem <markhannem3@gmail.com>
2022-11-28 20:56:49 -0330

    setting up api requests and crud commands

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 const services = require("../services/render");
+const controller = require("../controller/controller");
 
 // Root - GET
 route.get("/", services.homeRoutes);
@@ -12,6 +13,19 @@ route.get("/add-emp", services.add_emp);
 // Update Employee - GET
 route.get("/update-emp", services.update_emp);
 
+// API
+// add new employee
+route.post("/api/employees", controller.create);
+// fetch employee(s)
+route.get("/api/employees", controller.find);
+// update by employee id
+route.put("/api/employees/:id", controller.update);
+// delete an employee
+route.delete("/api/employees/:id", controller.delete);
+
+module.exports = route;
+
+// OLD CODE- Nov 26th 2022
 // Used callback function before changing it to services/render.js file
 
 // route.get("/", (req, res) => {
@@ -26,5 +40,3 @@ route.get("/update-emp", services.update_emp);
 // route.get("/update-emp", (req, res) => {
 //   res.render("update_emp");
 // });
-
-module.exports = route;
